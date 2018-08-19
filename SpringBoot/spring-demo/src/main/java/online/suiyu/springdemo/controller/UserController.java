1a6540f108d5513d368866f654d78e4617d14bfe
xiaozheng243 <luzz_1@163.com>
2018-08-19 16:33:46 +0800

    添加日志输出与slf4j

--- SpringBoot/spring-demo/src/main/java/online/suiyu/springdemo/controller/UserController.java
+++ SpringBoot/spring-demo/src/main/java/online/suiyu/springdemo/controller/UserController.java
@@ -1,15 +1,51 @@
 package online.suiyu.springdemo.controller;
 
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import online.suiyu.springdemo.entity.User;
+import online.suiyu.springdemo.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-public interface UserController {
-    String index();
+@Api(value = "User类")
+@RestController
+@RequestMapping(value = "/index", method = RequestMethod.GET)
+public class UserController {
 
-    List<User> getAllUser();
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    List<User> getAllUser(int pageNum, int pageSize);
+    @Autowired
+    private UserService userService;
 
-    List<User> getUserBySex(String sex);
+
+    @ApiOperation(value = "字符串返回数据测试", notes = "测试方法", tags = "hello")
+    @PostMapping(value = "/hello")
+    public String index() {
+        logger.info("文本测试");
+        return "Hello,SpringBoot";
+    }
+
+    @ApiOperation(value = "根据性别获取用户列表")
+    @PostMapping(value = "/getuserbysex")
+    public List<User> getUserBySex(@RequestParam(value = "sex", required = false, defaultValue = "man") String sex) {
+        return userService.getUserBySex(sex);
+    }
+
+    @ApiOperation(value = "获取所有的用户列表")
+    @PostMapping(value = "/getalluser")
+    public List<User> getAllUser() {
+        return userService.getAllUser();
+    }
+
+    @ApiOperation(value = "获取所有的用户列表")
+    @PostMapping(value = "/getalluser1")
+    public List<User> getAllUser(@RequestParam(value = "pageNum", required = false, defaultValue = "1") int pageNum,
+                                 @RequestParam(value = "pageSize", required = false, defaultValue = "4") int pageSize) {
+        return userService.getAllUser(pageNum, pageSize);
+    }
 }
