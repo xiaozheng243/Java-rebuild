0f447eb1c7800e8c3b2e79f2eed38808b71525bb
xiaozheng243 <luzz_1@163.com>
2018-08-27 21:55:53 +0800

    添加过滤器

---
+++ SpringBoot/spring-demo/src/main/java/online/suiyu/springdemo/filter/CustomFilter.java
@@ -0,0 +1,37 @@
+package online.suiyu.springdemo.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "customFilter", urlPatterns = {"/"})
+public class CustomFilter implements Filter {
+    private Logger logger = LoggerFactory.getLogger("CustomFilter.class");
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.info("CustomFilter过滤器启动");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        logger.info("doFilter 请求处理");
+        //对request、response进行一些预处理
+        // 比如设置请求编码
+        // request.setCharacterEncoding("UTF-8");
+        // response.setCharacterEncoding("UTF-8");
+        //TODO 进行业务逻辑
+
+        //链路 直接传给下一个过滤器
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("CustomFilter过滤器删除");
+    }
+}
