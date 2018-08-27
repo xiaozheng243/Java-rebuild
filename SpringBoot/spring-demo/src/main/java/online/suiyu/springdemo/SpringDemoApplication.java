d4d665dd863fe337af72890d81f9bb7c111d191c
xiaozheng243 <luzz_1@163.com>
2018-08-27 22:14:16 +0800

    添加过滤器

--- SpringBoot/spring-demo/src/main/java/online/suiyu/springdemo/SpringDemoApplication.java
+++ SpringBoot/spring-demo/src/main/java/online/suiyu/springdemo/SpringDemoApplication.java
@@ -3,7 +3,9 @@ package online.suiyu.springdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 @MapperScan("online.suiyu.springdemo.dao")
 public class SpringDemoApplication {
