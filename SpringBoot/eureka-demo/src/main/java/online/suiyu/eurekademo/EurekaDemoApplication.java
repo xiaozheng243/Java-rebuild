74cef7d802fa5ec21b97ed0f4f1ff84f4af1ec5c
xiaozheng243 <luzz_1@163.com>
2018-10-07 16:00:30 +0800

    创建Eureka Server项目

---
+++ SpringBoot/eureka-demo/src/main/java/online/suiyu/eurekademo/EurekaDemoApplication.java
@@ -0,0 +1,14 @@
+package online.suiyu.eurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDemoApplication.class, args);
+    }
+}
