079c15fa0465c0fc644f740c46f2fa46dbe65d7d
xiaozheng243 <luzz_1@163.com>
2018-11-25 10:32:20 +0800

    构建java基础项目

---
+++ javabasic/src/main/java/online/yuluo/javabasic/JavabasicApplication.java
@@ -0,0 +1,12 @@
+package online.yuluo.javabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavabasicApplication.class, args);
+    }
+}
