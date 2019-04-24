d730088da6df40c8f11d47ca112252f456a33baf
xiaozheng243 <luzz_1@163.com>
2019-04-24 20:46:34 +0800

    :tada: 未完成项目，因为rabbitmq没起作用，后续补全

---
+++ Gitbook/github/springclouddemo/spring-config-git-decoupling-refresh-bus/spring-client-git/src/main/java/online/yuluo/springclientgit/SpringClientGitApplication.java
@@ -0,0 +1,15 @@
+package online.yuluo.springclientgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringClientGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClientGitApplication.class, args);
+    }
+
+}
