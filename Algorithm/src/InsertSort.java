2bfbcd7f948305b05ea4326d4fbe764ca78f297a
xiaozheng243 <luzz_1@163.com>
2019-05-14 20:55:08 +0800

    增加插入排序

--- Algorithm/src/InsertSort.java
+++ Algorithm/src/InsertSort.java
@@ -1,2 +1,22 @@
+
 public class InsertSort {
+    public static void main(String[] args) {
+        int[] arrs = {70, 32, 5, 12, 93, 62, 21};
+        insertSort(arrs);
+        for (int a : arrs) {
+            System.out.println(a);
+        }
+    }
+
+    private static void insertSort(int[] arrs) {
+        for (int i = 1; i < arrs.length; i++) {
+            int index = i;
+            int temp = arrs[i];
+            while (index > 0 && temp < arrs[index - 1]) {
+                arrs[index] = arrs[index - 1];
+                index--;
+            }
+            arrs[index] = temp;
+        }
+    }
 }
