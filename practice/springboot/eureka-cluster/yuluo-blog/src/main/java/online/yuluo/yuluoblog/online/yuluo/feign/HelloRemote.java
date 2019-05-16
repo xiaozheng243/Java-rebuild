103f0af2d18d27067a40be36d462515f597ff6a7
xiaozheng243 <luzz_1@163.com>
2019-05-16 09:29:00 +0800

    添加primary注解

--- practice/springboot/eureka-cluster/yuluo-blog/src/main/java/online/yuluo/yuluoblog/online/yuluo/feign/HelloRemote.java
+++ practice/springboot/eureka-cluster/yuluo-blog/src/main/java/online/yuluo/yuluoblog/online/yuluo/feign/HelloRemote.java
@@ -3,10 +3,12 @@ package online.yuluo.yuluoblog.online.yuluo.feign;
 import online.yuluo.yuluoblog.online.yuluo.entity.UserInfoVO;
 import online.yuluo.yuluoblog.online.yuluo.feign.impl.HelloRemoteImpl;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Primary
 @FeignClient(name = "eureka-client",fallback = HelloRemoteImpl.class)
 public interface HelloRemote {
     @RequestMapping(value = "/user/getUserById/{id}", method = RequestMethod.GET)
