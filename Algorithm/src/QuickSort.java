8ca9abbc955199372e1ed893dc23e5cc9d19cbe1
xiaozheng243 <luzz_1@163.com>
2019-05-23 21:02:54 +0800

    算法之快速排序

--- Algorithm/src/QuickSort.java
+++ Algorithm/src/QuickSort.java
@@ -1,9 +1,40 @@
 public class QuickSort {
     public static void main(String[] args) {
 
+        int[] arrs = {12, 23, 34, 45, 56, 67, 1, 5, 3, 6};
+        quickSort(arrs, 0, arrs.length - 1);
+        for (int arr : arrs) {
+            System.out.println(arr);
+        }
     }
 
-    private void quickSort(int[] arrs) {
+    private static void quickSort(int[] arrs, int start, int end) {
+        if (start < end) {
+            int i = start, j = end;
+            int temp;
+            int target = arrs[start];
+            do {
+                while (arrs[i] < target && i < end) {
+                    i++;
+                }
+                while (arrs[j] > target && j > start) {
+                    j--;
+                }
+                if (i <= j) {
+                    temp = arrs[i];
+                    arrs[i] = arrs[j];
+                    arrs[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+            if (j > start) {
+                quickSort(arrs, start, j);
+            }
+            if (end > i) {
+                quickSort(arrs, i, end);
+            }
 
+        }
     }
 }
